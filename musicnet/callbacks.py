910f5e1bf74ceb3d54c0416093c37d2238e95e4e
serdyuk <serdyuk.dmitriy@gmail.com>
2017-05-14 16:58:16 -0400

    Add models and callbacks

---
+++ musicnet/callbacks.py
@@ -0,0 +1,89 @@
+from sklearn.metrics import precision_recall_curve, average_precision_score
+
+from keras.callbacks import Callback, ModelCheckpoint, LearningRateScheduler
+
+
+class SaveLastModel(Callback):
+    def __init__(self, workdir, period=10, name=''):
+        self.name = name
+        self.workdir = workdir
+        self.chkptsdir = os.path.join(self.workdir, "chkpts")
+
+        if not os.path.isdir(self.chkptsdir):
+            os.mkdir(self.chkptsdir)
+
+        self.period_of_epochs = period
+        self.link_filename = os.path.join(self.chkptsdir, 
+                                          "model_checkpoint.hdf5")
+    
+    def on_epoch_end(self, epoch, logs={}):
+        if (epoch + 1) % self.period_of_epochs == 0:
+            # Filenames
+            base_hdf5_filename = "model{}_checkpoint{:06d}.hdf5".format(
+                self.name, epoch + 1)
+            base_yaml_filename = "model{}_checkpoint{:06d}.yaml".format(
+                self.name, epoch + 1)
+            hdf5_filename = os.path.join(self.chkptsdir, base_hdf5_filename)
+            yaml_filename = os.path.join(self.chkptsdir, base_yaml_filename)
+            
+            # YAML
+            yaml_model = self.model.to_yaml()
+            with open(yaml_filename, "w") as yaml_file:
+                yaml_file.write(yaml_model)
+            
+            # HDF5
+            keras.models.save_model(self.model, hdf5_filename)
+            with h5py.File(hdf5_filename, "r+") as f:
+                f.require_dataset("initialEpoch", (), "uint64", True)[...] = int(epoch+1)
+                f.flush()
+            
+            # Symlink to new HDF5 file, then atomically rename and replace.
+            os.symlink(base_hdf5_filename, self.link_filename + ".rename")
+            os.rename (self.link_filename + ".rename",
+                       self.link_filename)
+
+
+class Performance(Callback):
+    def __init__(self, logger, **kwargs):
+        self.logger = logger
+        super(self, Performance).__init__(**kwargs)
+
+    def on_epoch_begin(self, epoch, logs={}):
+        self.timestamps = []
+
+    def on_epoch_end(self, epoch, logs={}):
+        t = np.asarray(self.timestamps, dtype=np.float64)
+        train_function_time = float(np.mean(t[ :,1] - t[:  ,0]))
+        load_data_time = float(np.mean(t[1:,0] - t[:-1,1]))
+        self.logger.log(
+            {'epoch': epoch, 'train_function_time': train_function_time})
+        self.logger.log({'epoch': epoch, 'load_data_time': load_data_time})
+
+    def on_batch_begin(self, epoch, logs={}):
+        self.timestamps += [[time(), time()]]
+
+    def on_batch_end(self, epoch, logs={}):
+        self.timestamps[-1][-1] = time()
+
+
+class Validation(Callback):
+    def __init__(self, x, y, name, logger):
+        self.x = x
+        self.y = y
+        self.name = name
+        self.logger
+
+    def on_train_begin(epochs, logs):
+        pr = self.model.predict(self.x[:, :, None])
+        average_precision  = average_precision_score(
+            self.y.flatten(), pr.flatten())
+        self.logger.log(
+            {'epoch': epoch, self.name + "_avg_precision": average_precision})
+
+    def on_epoch_end(epoch, logs):
+        pr = self.model.predict(self.x[:, :, None])
+        average_precision = average_precision_score(
+            self.x.flatten(), pr.flatten())
+        self.logger.log(
+            {'epoch': epoch, self.name + "_avg_precision": average_precision})
+
---
+++ musicnet/models.py
@@ -0,0 +1,113 @@
+import keras
+from keras.models import Model
+from keras.layers import (
+    Input, BatchNormalization, Activation, Dense, Flatten)
+from keras.layers.convolutional import Convolution1D, AveragePooling1D
+from keras.regularizers import l2
+
+from standard_resnet.vision_resnet import residual_block
+
+
+def get_mlp(d=4096, m=84):
+    model = keras.models.Sequential()
+    model.add(keras.layers.Flatten(input_shape=(d, 1)))
+    model.add(keras.layers.Dense(2048, activation='relu',
+                                 kernel_initializer='glorot_normal'))
+    model.add(keras.layers.Dense(m, activation='sigmoid',
+                                 kernel_initializer='glorot_normal',
+                                 bias_initializer=keras.initializers.Constant(value=0)))
+    model.compile(optimizer=keras.optimizers.Adam(lr=1e-3),
+                  loss='binary_crossentropy',
+                  metrics=['accuracy'])
+    return model
+
+
+def get_convnet(d=4096, m=84):
+    model = keras.models.Sequential()
+    model.add(keras.layers.Conv1D(
+        64, 512, strides=4, input_shape=(d, 1),
+        activation='linear',
+        kernel_initializer='glorot_normal'))
+    model.add(keras.layers.normalization.BatchNormalization(axis=-1))
+    model.add(keras.layers.Activation('relu'))
+    model.add(keras.layers.MaxPooling1D(pool_size=4, strides=2))
+
+    model.add(keras.layers.Conv1D(
+        32, 3, activation='relu',
+        kernel_initializer='glorot_normal'))
+    model.add(keras.layers.Conv1D(
+        32, 3,
+        activation='linear',
+        kernel_initializer='glorot_normal'))
+    model.add(keras.layers.normalization.BatchNormalization(axis=-1))
+    model.add(keras.layers.Activation('relu'))
+
+    #model.add(keras.layers.MaxPooling1D(pool_size=2))
+    #model.add(keras.layers.Conv1D(64, 3,
+    #                              kernel_initializer='glorot_normal'))
+    #model.add(keras.layers.normalization.BatchNormalization(axis=-1))
+    #model.add(keras.layers.Activation('relu'))
+
+    model.add(keras.layers.MaxPooling1D(pool_size=8))
+    model.add(keras.layers.Flatten())
+    model.add(keras.layers.Dense(2048, activation='relu',
+                                 kernel_initializer='glorot_normal'))
+    model.add(keras.layers.Dense(m, activation='sigmoid',
+                                 bias_initializer=keras.initializers.Constant(value=0)))
+    model.compile(optimizer=keras.optimizers.Adam(lr=1e-3),
+                  loss='binary_crossentropy',
+                  metrics=['accuracy'])
+    return model
+
+
+def get_music_resnet(inp_shape=(4096, 1), spatial_drop_prob=0, output_size=128):
+    '''This function returns the 50-layer residual network model
+    you should load pretrained weights if you want to use it directly.
+    Note that since the pretrained weights is converted from caffemodel
+    the order of channels for input image should be 'BGR' (the channel order of caffe)
+    '''
+
+    inp = Input(shape=inp_shape)
+    bn_axis = -1
+    filsize = 3 
+
+    out = Convolution1D(
+        filters=8, kernel_size=512, strides=4, activation='linear', padding='same', 
+        kernel_initializer='he_normal', use_bias=False, 
+        kernel_regularizer=l2(0.0001), name='conv1')(inp)
+    out = BatchNormalization(epsilon=1e-04, momentum=0.9, axis=bn_axis, name='bn_conv1')(out)
+    out = Activation('relu')(out)
+
+    num_blocks = 3
+    for i in range(num_blocks):
+        out = residual_block(
+            input_tensor=out, filter_size=filsize, featmaps=[8, 8], stage=2, 
+            strides=(4,),
+            block='{}'.format(i), shortcut='regular', 
+            spatial_drop_prob=spatial_drop_prob, dimensions=1, bn_axis=-1)
+
+    for i in range(num_blocks):
+        shortcut = 'projection' if i == 0 else 'regular'
+        out = residual_block(
+            input_tensor=out, filter_size=filsize, featmaps=[16, 16], stage=3,
+            strides=(4,),
+            block='{}'.format(i), shortcut=shortcut, spatial_drop_prob=spatial_drop_prob,
+            dimensions=1, bn_axis=-1)
+
+    for i in range(num_blocks):
+        shortcut = 'projection' if i == 0 else 'regular'
+        out = residual_block(
+            input_tensor=out, filter_size=filsize, featmaps=[32, 32], stage=4,
+            strides=(4,),
+            block='{}'.format(i), shortcut=shortcut, spatial_drop_prob=spatial_drop_prob,
+            dimensions=1, bn_axis=-1)
+
+    out = AveragePooling1D(pool_size=64)(out)
+    out = Flatten()(out)
+
+    out = Dense(output_size, activation='sigmoid', kernel_regularizer=l2(0.0001),
+                bias_initializer=keras.initializers.Constant(value=-5))(out)
+
+    model = Model(inp, out)
+
+    return model
