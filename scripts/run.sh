9fb55e09b5971f936fe39df53d92e3870241fd64
dmitriy-serdyuk <serdyuk.dmitriy@gmail.com>
2017-05-25 18:56:57 -0400

    Remove sh

--- scripts/run.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-
-# Dataset: /data/lisatmp3/speech/musicnet/musicnet_11khz.npz
-. $HOME/.bashrc
-pushd ..
-. ./env.sh
-popd
-
-train.py <model_name> --fourier --in-memory --model=deep_convnet --complex
